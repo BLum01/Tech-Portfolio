a9d440340e983ca0cd155129f9a5a153fced45af
hamsterglasses <brandon.lum650@gmail.com>
2024-05-14 11:38:18 -0700

    Code Comments

--- Sorting Algorithm Practice/src/Algorithms.java
+++ Sorting Algorithm Practice/src/Algorithms.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 
 public class Algorithms {
 	public static void main(String[]args) {
+
+		/* lines 13 to 22 test the sorting implementation chosen by creating a array with a random size, filling it with random numbers, and printing the unsorted and sorted array
+  		 * shows how I know how to create arrays and use Math.random()
+		 */
 		int arrSize = (int)(100*Math.random()+1);
 		int[] arr = new int[arrSize];
 		
@@ -17,10 +21,14 @@ public class Algorithms {
 		System.out.println(Arrays.toString(selectionSort(arr)));
 
 	}
-	
+
+	//insertion sorting method
 	public static int[] insertion(int[] array) {
 		for (int i = 0; i < array.length-1; i++){
 			int h = i;
+			/* I used a while loop here because I did not need to create another variable
+   			 * This shows my ability to implement different loops
+   			 */
 			while (h>=0 && array[h+1] < array[h] ) {
 				int temp = array[h+1];
 				array[h+1] = array[h];
@@ -33,7 +41,9 @@ public class Algorithms {
 		return array;
 	}
 	
-	
+	/* bubble sort sorting method
+ 	 * shows my ability to write recursive code
+	 */
 	public static int[] bubbleSort(int[] arr) {
 		for (int i = 0; i < arr.length-1; i++) {
 			if (arr[i] > arr[i+1]) {
@@ -42,6 +52,9 @@ public class Algorithms {
 				arr[i+1] = temp;
 			}
 		}
+		/* I used a private method because this code would be called multiple times, meaning that it
+  		 * would use a lot of memory if it was written in this method. It also makes the code easier to read.
+		 */
 		if(isSorted(arr)) {
 			return arr;
 		} else {
@@ -49,7 +62,9 @@ public class Algorithms {
 		}
 		return arr;
 	}
-	
+
+	/* selection sort sorting method
+	 */
 	public static int[] selectionSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			int max = arr[i];
@@ -70,7 +85,7 @@ public class Algorithms {
 	}
 	
 	//returns boolean if the array is sorted from least to greatest
-	public static boolean isSorted(int[] arr) {
+	private static boolean isSorted(int[] arr) {
 		for (int i = 0; i < arr.length-1; i++) {
 			if (arr[i] >  arr[i+1]) {
 				return false;
